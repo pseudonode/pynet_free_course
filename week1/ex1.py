ab3e964e3f6a2ff6b70575ed07f56a1732c61bd1
pseudonode <andre@recursivenet.com>
2018-05-09 19:21:51 +0000

    Week 1 start

---
+++ week1/ex1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+ip_addr1 = '1.1.1.1' 
+ip_addr2 = '2.2.2.2'
+ip_addr3 = '3.3.3.3'
+
+print (ip_addr1, ip_addr2, ip_addr3)
---
+++ week1/ex2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+ip_addr = input('Please insert a valid IP address:')
+print (ip_addr)
